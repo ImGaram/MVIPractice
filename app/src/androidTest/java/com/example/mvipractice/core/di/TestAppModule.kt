3b9460d28b4fc318b1427ea7e5778e638a344017
ImGaram <gim78014@gmail.com>
2024-12-18 23:22:54 +0900

    :white_check_mark: :: room, di 테스트 코드 통과

---
+++ app/src/androidTest/java/com/example/mvipractice/core/di/TestAppModule.kt
@@ -0,0 +1,24 @@
+package com.example.mvipractice.core.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.mvipractice.core.data.local.NoteDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDb(application: Application): NoteDb {
+        return Room.inMemoryDatabaseBuilder(
+            application,
+            NoteDb::class.java
+        ).build()
+    }
+}
