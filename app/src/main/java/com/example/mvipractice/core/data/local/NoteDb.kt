00ca4c47aa9a287fd123aae81eabeadc1055ae2a
ImGaram <gim78014@gmail.com>
2024-12-18 19:11:11 +0900

    :sparkles: :: room database 생성

---
+++ app/src/main/java/com/example/mvipractice/core/data/local/NoteDb.kt
@@ -0,0 +1,9 @@
+package com.example.mvipractice.core.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [NoteEntity::class], version = 1)
+abstract class NoteDb: RoomDatabase() {
+    abstract val noteDao: NoteDao
+}
