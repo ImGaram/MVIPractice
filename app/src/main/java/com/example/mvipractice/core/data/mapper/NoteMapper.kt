cb46ff1b2781a5e43940be5382edcd6c1f66eac1
ImGaram <gim78014@gmail.com>
2024-12-18 23:22:02 +0900

    :sparkles: :: room entity와의 mapping 함수 생성

---
+++ app/src/main/java/com/example/mvipractice/core/data/mapper/NoteMapper.kt
@@ -0,0 +1,33 @@
+package com.example.mvipractice.core.data.mapper
+
+import com.example.mvipractice.core.data.local.NoteEntity
+import com.example.mvipractice.core.domain.model.NoteItem
+
+fun NoteItem.toNoteEntityForInsert(): NoteEntity {
+    return NoteEntity(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded
+    )
+}
+
+fun NoteItem.toNoteEntityForDelete(): NoteEntity {
+    return NoteEntity(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded,
+        id = id
+    )
+}
+
+fun NoteEntity.toNoteItem(): NoteItem {
+    return NoteItem(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded,
+        id = id ?: 0
+    )
+}
---
+++ app/src/main/java/com/example/mvipractice/core/domain/model/NoteItem.kt
@@ -0,0 +1,9 @@
+package com.example.mvipractice.core.domain.model
+
+data class NoteItem(
+    var title: String,
+    var description: String,
+    var imageUrl: String,
+    val dateAdded: Long,
+    val id: Int = 0
+)
