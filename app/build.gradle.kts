606acd5e1beb5523bd7d87888d54514b8f4376dd
ImGaram <gim78014@gmail.com>
2024-12-18 23:20:52 +0900

    :heavy_plus_sign: :: 테스팅 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,16 +9,16 @@ plugins {
 
 android {
     namespace = "com.example.mvipractice"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mvipractice"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.example.mvipractice.HiltTestRunner"
     }
 
     buildTypes {
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
     // Room
-    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     androidTestImplementation(project(":app"))
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.paging)
@@ -62,7 +62,15 @@ dependencies {
 
     // Dagger Hilt
     implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
+    ksp(libs.androidx.hilt.compiler)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Hilt Test
+    testImplementation(libs.hilt.android.testing)
+    kspTest(libs.hilt.android.compiler)
+    androidTestImplementation(libs.hilt.android.testing)
+    kspAndroidTest(libs.hilt.android.compiler)
 
     // Retrofit
     implementation(libs.retrofit)
@@ -76,6 +84,20 @@ dependencies {
     // Extended Icons
     implementation(libs.androidx.material.icons.extended)
 
+    // Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.androidx.junit.ktx)
+    testImplementation(libs.core.ktx)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.truth)
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    androidTestImplementation(libs.truth)
+    androidTestImplementation(libs.androidx.core.testing)
+    androidTestImplementation(libs.core.ktx)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -84,7 +106,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
