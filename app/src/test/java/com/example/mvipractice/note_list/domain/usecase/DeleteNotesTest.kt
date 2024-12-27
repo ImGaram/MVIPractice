5828678042595e771c07e87a9852cd2794e00c0c
ImGaram <gim78014@gmail.com>
2024-12-27 22:13:36 +0900

    :white_check_mark: :: get, delete use case 테스트 코드 작성

---
+++ app/src/test/java/com/example/mvipractice/note_list/domain/usecase/DeleteNotesTest.kt
@@ -0,0 +1,43 @@
+package com.example.mvipractice.note_list.domain.usecase
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.mvipractice.core.data.repository.FakeNoteRepository
+import com.example.mvipractice.core.domain.model.NoteItem
+import com.example.mvipractice.noet_list.domain.usecase.DeleteNotes
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class DeleteNotesTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var fakeNoteRepository: FakeNoteRepository
+    private lateinit var deleteNotes: DeleteNotes
+
+    @Before
+    fun setUp() {
+        fakeNoteRepository = FakeNoteRepository()
+        deleteNotes = DeleteNotes(fakeNoteRepository)
+
+    }
+
+    @Test
+    fun `delete note from list, empty list`() = runTest {
+        fakeNoteRepository.shouldHaveFilledList(false)
+
+        val note = NoteItem(
+            "c title 2",
+            "desc 2",
+            "url2",
+            2
+        )
+
+        deleteNotes.invoke(note)
+
+        assertThat(fakeNoteRepository.getAllNotes().contains(note)).isFalse()
+    }
+}
