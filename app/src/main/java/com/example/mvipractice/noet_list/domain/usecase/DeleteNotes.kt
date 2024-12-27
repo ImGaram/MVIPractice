2a3b22c9914aafc39b5da244af1ca3053c955fc2
ImGaram <gim78014@gmail.com>
2024-12-27 22:12:11 +0900

    :sparkles: :: get, delete note use case 생성

---
+++ app/src/main/java/com/example/mvipractice/noet_list/domain/usecase/DeleteNotes.kt
@@ -0,0 +1,13 @@
+package com.example.mvipractice.noet_list.domain.usecase
+
+import com.example.mvipractice.core.domain.model.NoteItem
+import com.example.mvipractice.core.domain.repository.NoteRepository
+
+class DeleteNotes(
+    private val noteRepository: NoteRepository
+) {
+
+    suspend operator fun invoke(noteItem: NoteItem) {
+        noteRepository.deleteNote(noteItem)
+    }
+}
---
+++ app/src/main/java/com/example/mvipractice/noet_list/domain/usecase/GetAllNotes.kt
@@ -0,0 +1,16 @@
+package com.example.mvipractice.noet_list.domain.usecase
+
+import com.example.mvipractice.core.domain.model.NoteItem
+import com.example.mvipractice.core.domain.repository.NoteRepository
+
+class GetAllNotes(
+    private val noteRepository: NoteRepository
+) {
+    suspend operator fun invoke(isOrderByTitle: Boolean): List<NoteItem> {
+        return if (isOrderByTitle) {
+            noteRepository.getAllNotes().sortedBy { it.title.lowercase() }
+        } else {
+            noteRepository.getAllNotes().sortedBy { it.dateAdded }
+        }
+    }
+}
