30e4bdedcb6a47171b39c5c8f47fae8b9690e0f9
ImGaram <gim78014@gmail.com>
2025-01-04 01:58:52 +0900

    :white_check_mark: :: AddNoteViewModel 이미지 검색 로직 테스트

--- app/src/test/java/com/example/mvipractice/add_note/presentation/AddNoteViewModelTest.kt
+++ app/src/test/java/com/example/mvipractice/add_note/presentation/AddNoteViewModelTest.kt
@@ -1,7 +1,10 @@
 package com.example.mvipractice.add_note.presentation
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.mvipractice.MainCoroutineRule
+import com.example.mvipractice.add_note.domain.usecase.SearchImages
 import com.example.mvipractice.add_note.domain.usecase.UpsertNote
+import com.example.mvipractice.core.data.repository.FakeImagesRepository
 import com.example.mvipractice.core.data.repository.FakeNoteRepository
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.test.runTest
@@ -14,14 +17,20 @@ class AddNoteViewModelTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
     private lateinit var addNoteViewModel: AddNoteViewModel
     private lateinit var fakeNoteRepositoryImpl: FakeNoteRepository
+    private lateinit var fakeImagesRepository: FakeImagesRepository
 
     @Before
     fun setUp() {
         fakeNoteRepositoryImpl = FakeNoteRepository()
+        fakeImagesRepository = FakeImagesRepository()
         val upsertNote = UpsertNote(fakeNoteRepositoryImpl)
-        addNoteViewModel = AddNoteViewModel(upsertNote)
+        val searchImages = SearchImages(fakeImagesRepository)
+        addNoteViewModel = AddNoteViewModel(upsertNote, searchImages)
     }
 
     @Test
@@ -55,5 +64,31 @@ class AddNoteViewModelTest {
         )
 
         assertThat(isInserted).isTrue()
+    }
+
+    @Test
+    fun `search image with empty query, image list is empty`() = runTest {
+        addNoteViewModel.searchImages("")
+        mainCoroutineRule.dispatcher.scheduler.advanceUntilIdle()
+
+        assertThat(addNoteViewModel.addNoteState.value.imageList.isEmpty()).isTrue()
+    }
+
+    @Test
+    fun `search image with a valid query but with error, image list is empty`() = runTest {
+        fakeImagesRepository.setShouldReturnError(true)
+
+        addNoteViewModel.searchImages("query")
+        mainCoroutineRule.dispatcher.scheduler.advanceUntilIdle()
+
+        assertThat(addNoteViewModel.addNoteState.value.imageList.isEmpty()).isTrue()
+    }
+
+    @Test
+    fun `search image with a valid query, image list is not empty`() = runTest {
+        addNoteViewModel.searchImages("query")
+        mainCoroutineRule.dispatcher.scheduler.advanceUntilIdle()
+
+        assertThat(addNoteViewModel.addNoteState.value.imageList.isNotEmpty()).isTrue()
     }
 }
