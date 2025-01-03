626678ff52e9903110276f927008276be74befe8
ImGaram <gim78014@gmail.com>
2025-01-04 00:08:30 +0900

    :sparkles: :: pixabay api response mapping 함수 생성

---
+++ app/src/main/java/com/example/mvipractice/core/data/mapper/ImageMapper.kt
@@ -0,0 +1,10 @@
+package com.example.mvipractice.core.data.mapper
+
+import com.example.mvipractice.core.data.remote.dto.ImageListDTO
+import com.example.mvipractice.core.domain.model.Images
+
+fun ImageListDTO.toImages(): Images {
+    return Images(
+        images = hits?.map { it.imageURL ?: "" } ?: emptyList()
+    )
+}
---
+++ app/src/main/java/com/example/mvipractice/core/domain/model/Images.kt
@@ -0,0 +1,5 @@
+package com.example.mvipractice.core.domain.model
+
+data class Images(
+    val images: List<String>
+)
