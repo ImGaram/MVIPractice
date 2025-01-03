4657de0e17779d1c4b4c42eb9b1bb348fa155d5b
ImGaram <gim78014@gmail.com>
2025-01-04 00:08:15 +0900

    :sparkles: :: pixabay api request 생성

---
+++ app/src/main/java/com/example/mvipractice/core/data/remote/api/ImagesApi.kt
@@ -0,0 +1,19 @@
+package com.example.mvipractice.core.data.remote.api
+
+import com.example.mvipractice.core.data.remote.dto.ImageListDTO
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ImagesApi {
+
+    @GET("/api/")
+    suspend fun searchImages(
+        @Query("q") query: String,
+        @Query("key") apiKey: String = API_KEY
+    ): ImageListDTO?
+
+    companion object {
+        const val BASE_URL = "https://pixabay.com"
+        const val API_KEY = "xxx"
+    }
+}
